b23f352bebefe980e3960f84e098b4461f1f1e06
mateusz <mateuszalmannai@googlemail.com>
2017-05-31 17:30:06 +0200

    modify styling and add comment

--- app/models/folder.js
+++ app/models/folder.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  // array of files: attribute left blank so that it is accepted
+  // as an array and not to try to push it into anything
   files: DS.attr()
 });
